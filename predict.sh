97842c712021f2d903019d16276631ef7045d5e3
stephanie3han <stephanie3han@gmail.com>
2020-05-11 13:19:14 +0200

    changement de la file predict.sh et number_lenet.cfg

--- predict.sh
+++ predict.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./darknet/darknet classifier predict cfg/number.dataset cfg/number_lenet.cfg ./backup/number_lenet.weights $1 #data/number/images/v_00000_c7.png
+./darknet/darknet classifier predict cfg/number.dataset cfg/number_lenet.cfg ./backup/number_lenet_5965.weights  #./backup/number_lenet.weights $1 #data/number/images/v_00000_c7.png 
